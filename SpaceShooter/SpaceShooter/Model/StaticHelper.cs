babbb19e5b2e604e4ef1d0684412330b7b7fcee5
aggepagge <agges_allamo@hotmail.com>
2014-01-09 02:01:17 +0100

    med allt...

---
+++ SpaceShooter/SpaceShooter/Model/StaticHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SpaceShooter.Model.GameComponents.Weapons;
+
+namespace SpaceShooter.Model
+{
+    public static class StaticHelper
+    {
+        public static float getFireRate(WeaponTypes weapon)
+        {
+            switch(weapon)
+            {
+                case WeaponTypes.Raygun:
+                    return 0.1f;
+                case WeaponTypes.Missile:
+                    return 0.4f;
+                default:
+                    return 0.1f;
+            }
+
+        }
+    }
+}
